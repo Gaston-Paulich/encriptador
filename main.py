c22e3a2fe8cce89fd4ce5322aba74bb665217f94
Gaston Paulich <gastopau@quilmes.com.ar>
2022-04-13 14:31:44 -0300

    se agrega el proyecto

---
+++ main.py
@@ -0,0 +1,54 @@
+
+
+def encriptar(texto):
+    print('el texto a encriptar es: ' + texto)
+    textoFinal = ''
+    for letra in texto:
+        ascii = ord(letra)
+        ascii += 1
+        textoFinal += chr(ascii)
+    return textoFinal
+
+def desencriptar(texto):
+    print('el texto a desencriptar es: ' + texto)
+    textoFinal = ''
+    for letra in texto:
+        ascii = ord(letra)
+        ascii -= 1
+        textoFinal += chr(ascii)
+    return textoFinal
+
+
+
+def encriptarArchivo(rutaArchivo):
+    archivo = open(rutaArchivo, 'r')
+    texto = archivo.read()
+    archivo.close()
+    textoEncriptado = encriptar(texto)
+
+    archivo = open(rutaArchivo, 'w')
+    archivo.write(textoEncriptado)
+    archivo.close()
+    print('El archivo se encripto correctamente')
+
+def desencriptarArchivo(rutaArchivo):
+    archivo = open(rutaArchivo, 'r')
+    texto = archivo.read()
+    archivo.close()
+    textoDesEncriptado = desencriptar(texto)
+
+    archivo = open(rutaArchivo, 'w')
+    archivo.write(textoDesEncriptado)
+    archivo.close()
+    print('El archivo se desencripto correctamente')
+
+
+
+respuestaEoD = input('Presione "E" para encriptar, o "D" para desencriptar: ')
+
+rutaArchivo = input('Ingrese la ruta del archivo: ')
+
+if respuestaEoD == 'E' or respuestaEoD == 'e':
+    encriptarArchivo(rutaArchivo)
+else:
+    desencriptarArchivo(rutaArchivo)
